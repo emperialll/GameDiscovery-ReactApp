6108ed6ff197b515f716170ef43ede314068a533
Shahriar <sh.fa84@gmail.com>
2023-11-20 03:19:07 +0100

    Create generic data fetching hook

--- Game-Hub/src/hooks/useGames.ts
+++ Game-Hub/src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Platform {
     id: number;
@@ -16,37 +14,6 @@ export interface Game {
     metacritic: number;
   }
   
-   interface fetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-  
-const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]); // state variable to store game objects
-    const [error, setError] = useState(""); // state variable to store errors
-    const [isLoading, setLoading] = useState(false);
-  
-    // useEffect to send fetch request to backend
-    useEffect(() => {
-        const controller = new AbortController();
-
-        setLoading(true);
-        apiClient
-        .get<fetchGamesResponse>("/games", {signal: controller.signal})
-        .then((res) => {
-            setGames(res.data.results)
-            setLoading(false)
-        })
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)
-            setLoading(false);
-        });
-
-      return () => controller.abort();
-    }, []);
-
-    return {games, error, isLoading}
-}
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
--- Game-Hub/src/hooks/useGenres.ts
+++ Game-Hub/src/hooks/useGenres.ts
@@ -1,43 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
     id: number;
     name: string;
 }
 
-interface fetchGenresResponse {
-    count: number;
-    results: Genre[];
-}
-
-const useGenres = () => {
-    const [genres, setGenres] = useState<Genre[]>([]); // state variable to store game objects
-    const [error, setError] = useState(""); // state variable to store errors
-    const [isLoading, setLoading] = useState(false);
-  
-    // useEffect to send fetch request to backend
-    useEffect(() => {
-        const controller = new AbortController();
-
-        setLoading(true);
-        apiClient
-        .get<fetchGenresResponse>("/genres", {signal: controller.signal})
-        .then((res) => {
-            setGenres(res.data.results)
-            setLoading(false)
-        })
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)
-            setLoading(false);
-        });
-
-      return () => controller.abort();
-    }, []);
-
-    return {genres, error, isLoading}
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
